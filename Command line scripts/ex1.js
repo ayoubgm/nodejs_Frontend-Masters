dd2a069cea5c07845df27bbc8a460e7b44b21797
Guismi Ayoub <gayoub@e1r9p1.1337.ma>
2020-02-22 10:53:31 +0100

    begin for node.js

---
+++ Command line scripts/ex1.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+"use strict";
+
+/*
+    About line (3) "use strict", this string defines the javascript
+    code should be executed in strict mode.
+
+    So, Why Strict mode ?
+    -> Strict mode makes it easier to write secure javascript.
+    -> mistyping a variable name creates a new variable, in
+    strict mode will throw an error, making it impossible to accidentlly create 
+    a global variable.
+
+*/
+
+//process.stdin.read()
+
+
+console.log("Hello world");
+console.error("Oops !");
---
+++ ex1.js
@@ -0,0 +1,15 @@
+/*
+    -> to run this js file, you can simply run the following command :
+        node file_name.js
+*/
+
+// process.stdout.write("Hello world\n");
+console.log("Hello world");
+
+
+/*  
+    So if you run the two lines (6, 7) you see something diffrent on command line,
+    so we see that process.stdout.write will print the string as she write just 
+    caracters, so we understand the console.log() is bunch of function alot of things
+    works to show us the output !
+*/
